69160fd4b2765405b41c4592c8f1d1a7082b5aed
Hieu Truong <giahieu0201@gmail.com>
2025-06-27 22:10:53 +0700

    Update code

--- Ecommerce/Services/Catalog/Catalog.Application/Response/BrandResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Catalog.Application.Response
-{
-    public class BrandResponse
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
-
--- Ecommerce/Services/Catalog/Catalog.Application/Response/TypeResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Catalog.Application.Response
-{
-    public class TypeResponse
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
-
