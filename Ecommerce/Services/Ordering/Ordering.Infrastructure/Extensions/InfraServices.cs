427ffdb5db5b10838c29c4055f6fb6336df12230
Hieu Truong <giahieu0201@gmail.com>
2025-12-07 22:59:48 +0700

    Fixbug build service

--- Ecommerce/Services/Ordering/Ordering.Infrastructure/Extensions/InfraServices.cs
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/Extensions/InfraServices.cs
@@ -11,8 +11,7 @@ namespace Ordering.Infrastructure.Extensions
     {
         public static IServiceCollection AddInfraServices(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
-            serviceCollection.AddDbContext<OrderContext>(options => options.UseSqlServer(
-                configuration.GetConnectionString("OrderingConnectionString")));
+            serviceCollection.AddDbContext<OrderContext>(options => options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
             serviceCollection.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
             serviceCollection.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
             return serviceCollection;
